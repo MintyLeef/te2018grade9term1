151619736a153ffc66d98f437f08aa5994f8a2b5
Neil <neil.baidan@csedge.org>
2018-10-06 15:45:43 -0400

    Completed Challenge 1

--- data.js
+++ data.js
@@ -1,5 +1,23 @@
-// Example
-let message = "Hello World!";
-console.log(message);
+let hp = 150;
+let stamina = 200;
+let speed = 112;
 
-// Type your code below!
+let player1 = ("The Hero");
+let player2 = ("Companion");
+let enemy = ("Enemy");
+
+
+let respond = true;
+let respond2 = false;
+
+let name;
+
+console.log("player1: Are you ready?");
+console.log("player2: Yup");
+console.log("enemy: You can't run!!!");
+
+const READLINE = require("readline-sync");
+
+let userInput = READLINE.question("Type anything and I'll repeat back!");
+
+console.log(userinput);
