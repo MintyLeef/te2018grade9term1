e310c9a33b9e6fb5ba729c755816bea067c5d5b8
Neil <neil.baidan@csedge.org>
2018-10-20 12:43:54 -0400

    Objects: Hacker and Villain

---
+++ object.js
@@ -0,0 +1,21 @@
+let spy;
+spy = {
+	first: "Bo",
+	last: "Loon",
+	nickName: "Boon",
+	catchPhrase: "R333",
+	archEnemy: "G0N"
+	};
+
+let villain;
+villain = {
+	first:"Po",
+	last: "Per",
+	nickName: "G0N",
+	catchPhrase: "Yeet",
+	crime: "destroyed Despacito 4"
+	};
+
+console.log(`Salutations I am ${spy.last}, ${spy.first} ${spy.last}. Some call me ${spy.nickName} and my catchphrase is ${spy.catchPhrase}`);
+
+console.log(`Hehehe I'm Po Per but you can call me ${villain.first} ${villain.last} but you can call me ${villain.nickName}. My greatest crime is ${villain.crime} and I always say ${villain.catchPhrase}`);
