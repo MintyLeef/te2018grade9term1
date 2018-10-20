4e9a638f122a1f58f683366e391b0b428b131ce5
Neil <neil.baidan@csedge.org>
2018-10-20 15:43:22 -0400

    Change Final

--- change.js
+++ change.js
@@ -1,26 +1,40 @@
 // import readline-sync module, don't touch!
 const READLINE = require("readline-sync");
-let inputCents = READLINE.question("Welcome Human. Please input positive cent value")
+
 // fill in object below with all relevant properties (see instructions)
 
 
 // write code below that simulates the change machine in action!
 let changeMachine = {
-	quarters: 'undefined',
-	dimes: 'undefined',
-	nickels: 'undefined',
-	pennies: 'undefined'
+	inputCents: undefined,
+	quartCnt: undefined,
+	dimesCnt: undefined,
+	nickelsCnt: undefined,
+	penniesCnt: undefined,
+	goodbye: "Goodbye Human",
+	greetng: "Welcome Human"
 };
 
+changeMachine.inputCents = READLINE.question("Welcome Human. Please input positive cent value:  ");
 
-quartCnt = Math.floor(inputCents / 25)
-d = Math.floor(inputCents % 25)
-dimesCnt = Math.floor(d/10)
-n = Math.floor(d%10)
-nicklesCnt = Math.floor(n/5)
-p = Math.floor(n%5)
+changeMachine.quartCnt = Math.floor(changeMachine.inputCents / 25);
+let q = changeMachine.inputCents % 25;
 
-console.log(`${quartCnt}, ${d}, ${dimesCnt}, ${n}, ${nicklesCnt}, ${p}`);
+
+changeMachine.dimesCnt = Math.floor(q / 10);
+let q2 = q % 10;
+
+changeMachine.nickelsCnt = Math.floor(q2 / 5);
+
+changeMachine.penniesCnt = q2 % 5;
+
+console.log(`
+quarters = ${changeMachine.quartCnt} 
+dimes = ${changeMachine.dimesCnt}
+nickels =  ${changeMachine.nickelsCnt} 
+pennies = ${changeMachine.penniesCnt} 
+${changeMachine.goodbye}`);
+
 
 
 
