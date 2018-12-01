e0d5246f6c73fa69b9fb542afe8b569ce3c3b545
MintyLeaf <neil.baidan@gcschool.org>
2018-11-30 22:10:53 -0800

    DUNEGON TRUE

---
+++ conditionals.js
@@ -0,0 +1,132 @@
+const READLINE = require("readline-sync");
+
+// **** Problem 1: R-rated ****
+// You cannot see an R-rated movie unless you are at least 18, or you are with
+// an adult. Write code that prints whether or not someone can see an
+// R-rated movie.
+console.log("*** Problem 1: R-rated ***");
+let age = READLINE.question("Enter your age: ");
+ if(age >= 18 ) {
+ 	console.log("Congrats you can see the movie")
+ }
+else{
+	let withAdult = READLINE.question("Are you with an adult? (yes or no): ");
+	if (withAdult == "no"){
+		console.log("Sorry but you can't see the movie")
+		if(withAdult == "yes") {
+			console.log("Go on ahead")
+		}
+	}
+}
+
+ 
+
+// **** Problem 2: Umbrella ****
+// You should bring an umbrella when you travel, but only if it is raining.
+// However, if it is thunderstorming, you should not bring an umbrella since
+// that's bad luck. Write code that prints if someone should bring an umbrella
+// with them.
+console.log("*** Problem 2: Umbrella ***");
+let raining = READLINE.question("Is it raining? (yes or no): ");
+let thunderstorming = READLINE.question("Is it thunderstorming? (yes or no): ");
+ if(raining = "yes" && thunderstorming = "no") {
+ 	console.log("I should bring my umbrella");
+ }
+	if(thunderstorming = "yes" && raining = "no"){
+		console.log("I won't take my umbrella");
+	}
+
+
+
+
+// **** Problem 3: Monkey Trouble ****
+// There are two monkeys: Bubbles and Spankey. You are in trouble if both of
+// them are smiling, or if neither are smiling. Write code that prints if we
+// are in trouble.
+console.log("*** Problem 3: Monkey Trouble ***");
+let bubbles = READLINE.question("Is Bubbles smiling? (yes or no): ");
+
+let spankey = READLINE.question("Is Spankey smiling? (yes or no): ");
+if(bubbles == "yes" && spankey == "yes") {
+	console.log("You better run. Bubbles and Spankey are smiling maliciously");
+}
+else{                       
+	console.log("Ok we are safe")
+}
+
+// **** Problem 4: First Place ****
+// Write code that prints the largest of three scores. If there is a tie for
+// first place, simply print one of the tie winners.
+console.log("*** Problem 4: First Place ***");
+let score1 = READLINE.question("Enter the first score: ");
+let score2 = READLINE.question("Enter the second score: ");
+let score3 = READLINE.question("Enter the third score: ");
+if (score1 > score2 && score1 > score3){
+	console.log($)
+}
+
+if (score2 > score1 && score2 > score3 ) {
+	console.log("Congrats you are in 2nd place")
+}
+
+if (score3 > score2 && score3 > 1) {
+	console.log("Congrats you are in 3rd place")
+}
+	
+
+
+
+// **** Problem 5: Phone Shopping ****
+// At a phone store, you can afford various "tiers" of phones based on how
+// much money you have.
+//    If you have at least $30 you can buy a prepaid phone.
+//    If you have at least $100 you can buy a bottom-tier phone.
+//    If you have at least $300 you can buy a middle-tier phone.
+//    If you have at least $600 you can buy a top-tier phone.
+// Write code that prints all the phone tiers someone can buy based on how much
+// money they have.
+console.log("*** Problem 5: Phone Shopping ***");
+let money = READLINE.question("How much money do you have?: ");
+if (money >= 30) {
+	console.log("You can buy a 30$ pre-paid phone");
+	if (money >=100) {
+		console.log("You can buy a 100$ bottom-tier phone");
+		if (money >= 300) {
+			console.log("You can buy a 300$ middle-tier phone");
+			if (money >= 600) {
+				console.log ("You can buy a 600$ top-tier")
+			}
+
+		}
+
+	}
+}
+
+
+// **** Problem 6: Guess My Number ****
+// Write code that plays a simple number guessing game with a user. You can
+// use any number you like as myNum. If the user guesses the number, print
+// a congratulations message. If they guess too low or too high, tell them this.
+// If their guess is myNum +/- 3, tell them they are "warm"; otherwise tell
+// them they are cold.
+console.log("*** Problem 6: Guess My Number ***");
+let myNum = 5;
+let guess = READLINE.question("I'm thinking of a number. Guess it: ");
+if (guess <= myNum); {
+	console.log("Cold")
+}
+if (fuess >= myNum); {
+	console.log("Warm")
+}
+if (guess = 5); {
+	console.log(CONGRATS);
+}
+
+
+
+
+
+
+
+
+
--- dungeontrue.js
+++ dungeontrue.js
@@ -1,11 +1,18 @@
 const READLINE = require("readline-sync");
 let player;
-
+let attackWolf;
+let fleeWolf;
+let doorHand;
+let finalDrake;
+let attackDrake;
+let smartMove;
+let dumbMove;
+let talkDrake;
 
 //START
 console.log("*** WELCOME TO THE DUNGEON ***");
 let startscreen = READLINE.question("START/EXIT:");
-if(startscreen == "START") {
+if(startscreen == "START" || startscreen == "start") {
   console.log("Starting Game...")
 }
  else{
@@ -13,75 +20,173 @@ if(startscreen == "START") {
           console.log("Come Back Again!")
         }
 
-if (startscreen == "START") {
+if (startscreen == "START" || startscreen == "start") {
 
 
 
 //CLASS SELECTION
-console.log("Choose Your Class: MAGE__SWORDSMAN__ARCHER")
+console.log("Choose Your Class: MAGE__SWORDSMAN")
  role = READLINE.question("What is your class?:");
  
 
 //MAGE ROUTE
-if(role == "MAGE"){
+if(role == "MAGE" || role == "mage"){
   console.log(
-  "Name = Arlen",
-  "Courage(CRG) = 10",
-  "Intellect(INT) = 17",
-  "Strength(STR) = 3",
-  "Magic(MAG) = 18.5",
-  "Weapon = Magic Staff"
+  "| Name = Arlen |",
+  " Courage(CRG) = 10 |",
+  " Intellect(INT) = 17 |",
+  " Strength(STR) = 3 |",
+  " Magic(MAG) = 18.5 |",
+  " Weapon = Magic Staff |"
   )
 }
 
-if (role == "MAGE") {
-  player = 
-  name = "Arlen",
-  courage = 7,
-  intellect = 17,
-  strength = 3,
-  magic = 18.5,
-  weapon = "Magic Staff"
+if (role == "MAGE" || role == "mage") {
+  player = {
+  name: "Arlen",
+  courage:10,
+  intellect:17,
+  strength: 3,
+  magic: 18.5,
+  weapon: "Magic Staff"
+}
 };
-if(role == "MAGE"){
+if(role == "MAGE" || role == "mage"){
   console.log("Arlen is a powerful wizard's apprentice. He is tasked with retrieving a rare gemstone capable of powering the Hamlet, from the dungeon");
 
 }
 
 //SWORDSMAN ROUTE
-if(role == "SWORDSMAN") {
-console.log("Name = Anton",
-  "| Courage(CRG) = 18 |",
-  "| Intellect(INT) = 13 |",
-  "| Strength(STR) = 19 |",
-  "| Magic(MAG) = 0 |",
-  "| Weapon = Sword and Shield |")
+if(role == "SWORDSMAN" || role == "swordsman") {
+console.log("| Name = Anton |",
+  " Courage(CRG) = 18 |",
+  " Intellect(INT) = 13 |",
+  " Strength(STR) = 19 |",
+  " Magic(MAG) = 0 |",
+  " Weapon = Sword and Shield |")
 }
 
-if (role == "SWORDSMAN") {
+if (role == "SWORDSMAN" || role == "swordsman") {
+  player = {
   name: "Anton",
-  courage = 18,
-  intellect = 13  ,
-  strength = 19,
-  magic =  0,
-  weapon = "Sword and Shield"
+  courage: 18,
+  intellect: 13  ,
+  strength: 19,
+  magic: 0,
+  weapon:"Sword and Shield"
 };
+}
 
-if (role == "SWORDSMAN") {
-  console.log("Anton is an orphaned child who was adopted by a farming family. His adopted parents told tales of his adventurous father and mother. He seeks to make a name for himself in the dungeon.")
+if (role == "SWORDSMAN"|| role == "swordsman") {
+  console.log("Anton is an orphaned child who was adopted by a farming family. His adopted parents told tales of his adventurous father and mother. He seeks to make a name for himself in the dungeon by slaying the famed drake.");
 }
 
 //DUNGEON ENTER
 
-let enterDungeon = READLINE.question("Will you enter dungeon? (yes or no) ");
-if(enterHouse == "yes") {
-  console.log(`You prepare your ${player.weapon} and enter the maw of the dungeon...`);
-
+let enterDungeon = READLINE.question("Will you enter dungeon? (yes or no): ");
+if(enterDungeon == "yes") {
+  console.log(`You prepare your mind and enter the maw of the dungeon...`);
 } 
+
 else {
-  player.intellect++;
   console.log(`You coward. Return when you've grown. Pathetic`);
+}
+//DUNGEON START MAGE
+if (role == "MAGE" || role == "mage") {
+  console.log("After a few minutes of walking in the dark and eery dungeon a wolf appears")
+ attackWolf = READLINE.question("What will you do? (Fight, Talk, Flee):");
+}
+
+if (attackWolf == "Fight") {
+  console.log("You charge your magic staff and hit the wolf directly between the eyes, singing its fur. Startled but determined, you walk on.");
+  player.courage += 2
+}
+
+
+if (attackWolf == "Talk") {
+  console.log("It's no use. The beast cannot understand your words. The wolf charges at you and bites your leg.");
+fleeWolf = READLINE.question("What will you do? (Flee):");
+}
+
+if (fleeWolf == "Flee"){
+    console.log("You run for your life, the wolf soon losing track of you.");
+     player.courage -= 2;
+    console.log("SYSTEM: YOU LOSE 2 CRG. YOU NOW HAVE 8 CRG");
+    console.log(`${player.courage}`)
+  }
+
+//MAGIC DOOR(MAGE)
+if (role == "MAGE" || role == "mage") {
+  console.log("You come upon a door with a faint glow coming from the other side. This must be the GEM ROOM! There are 2 hand shaped spots on the door.");
+  doorHand = READLINE.question("Do you place your hands? (yes or no):");
+}
+if (doorHand == "yes" && player.courage < 9){
+  console.log("SYSTEM: YOU CANNOT OPEN THE DOOR. YOU DO NOT HAVE ENOUGH COURAGE. REQUIRED: 9 CRG ");
+}
+
+
+if (doorHand == "yes" && player.courage >= 9){
+  console.log("The door starts to glow and it opens. A shiny gem sits on a pillar. You take the gem and thus complete your mission");
+}
+
+if (doorHand == "no"){
+  console.log("The door is the only way to the gem. Comeback when you no longer fear the dungeon.")
+}
+
+
+
+//DUNGEON START SWORDSMAN
+if (role == "SWORDSMAN" || role == "swordsman") {
+  console.log("You smell burning in the air. It must be the DRAKE. You charge deeper into the cave and see the fearsome beast. ");
+ attackDrake = READLINE.question("What will you do? (Fight, Talk, Flee):")
+}
+if (attackDrake == "Fight" || attackDrake == "fight"){
+  console.log("You strike the DRAKE's foot and it screams in pain")
+  finalDrake = READLINE.question("The DRAKE retaliates by sending a fireball towards you. What will you do? (Sword, Shield):")
+}
+if (finalDrake == "Sword" || finalDrake == "sword"){
+  console.log("You run your sword through the fireball. You take damage,severely wounding your Arm")
+  player.strength -= 9.5;
+  console.log("SYSTEM: YOU LOSE 9.5 STR. YOU NOW HAVE 9.5 STR");
+  console.log(`${player.strength}`);
+dumbMove = READLINE.question("The Dragon rears back for another attack. What will you do? (Fight, Flee):")
+}
+
+if (dumbMove == "Fight" && player.strength < 10 || dumbMove == "fight" && player.strength < 10){
+  console.log("You muster what strength you have to lift your blade but without your other arm you can't. The DRAKE rams you and kills you.")
 
 }
+if (dumbMove == "Flee" || dumbMove == "flee") {
+  console.log("You run away like a coward");
+}
+
+if (finalDrake == "Shield" || finalDrake == "shield"){
+  console.log("You stand in place, positioning your shield in front of you. The fireball slams against your shiled and deals 0 damage")
+  player.intellect += 3;
+  console.log("SYSTEM: YOU GAINED 3 INT. YOU NOW HAVE 16 STR");
+  console.log(`${player.intellect}`);
+  smartMove = READLINE.question("The Dragon rears back for another attack. What will you do? (Fight, Flee):")
+}
+
+if (smartMove == "Fight" || smartMove == "fight"){
+  console.log("You run at the DRAKE and pierce through its heart, killing the creature once and for all. You return home with the horns of the DRAKE. The townspeople cheer your name.")
+}
+if (smartMove == "Flee" || smartMove == "flee") {
+  console.log("You run away like a coward")
+}
+
+if (attackDrake == "Talk" || attackDrake == "talk"){
+  console.log("You yell at the  beast and it snarls back");
+  talkDrake = READLINE.question("What will you do? (Fight, Flee):");
+}
+
+if (attackDrake == "Flee" || attackDrake == "flee"){
+  console.log("You run away like a coward");
+  player.courage -= 5;
+  console.log("SYSTEM: YOU LOSE 5 CRG. YOU NOW HAVE 13 CRG");
+  console.log(`${player.courage}`);
+}
+
 console.log("Thanks for playing!");
+
 }
